d3cd6395958bdcfd214951ae74862734d3edd30b
Pavana Prana Das Brahmachari <pvpd@ISKCONMys.local>
2021-04-21 15:51:11 +0530

    working!

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 var e = document.getElementById("slide")
 e.textContent=text.verses[0].roman
 
-function rep(ev){
+function handleKey(ev){
     if(ev.key=="ArrowRight" || ev.key=="ArrowLeft"){
         replaceScript(ev)
         updateVerse()
     }
 }
 
-document.addEventListener('keydown',rep)
+document.addEventListener('keydown',handleKey)
 
 function replaceScript(ev){
-
-
     document.getElementById("text").remove()
     var s = document.createElement("script")
     s.setAttribute("id","text")
-
     var path
     if(ev.key=="ArrowRight"){
         path = text.info.nextId
@@ -24,11 +21,10 @@ function replaceScript(ev){
     if(ev.key=="ArrowLeft"){
         path = text.info.prevId
     }
-
-        s.setAttribute("src","./books/js/"+ path + ".js")
-        s.setAttribute("type","text/javascript")
-        document.getElementById("textscript").appendChild(s)
-    
+    s.setAttribute("src","./books/js/"+ path + ".js")
+    s.setAttribute("type","text/javascript")
+    document.getElementById("textscript").appendChild(s)
+    s.addEventListener('load',updateVerse);
 }
 
 function updateVerse(){
